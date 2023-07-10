5f81417984f2577e6af0ffe09bcc6899a9ff1872
WojciechWnukk <wojciech.wnukk@gmail.com>
2023-07-10 15:41:23 +0200

    vercel v2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/*
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -32,3 +33,33 @@ app.use("/api/auth", authRoutes)
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
+*/
+// server.js
+
+const express = require('express')
+const cors = require('cors')
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+const orderRoutes = require("./routes/orders")
+const employeeRoutes = require("./routes/employees")
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const tokenVerification = require('./middleware/tokenVerification')
+
+// routes
+app.use("/api/employees", employeeRoutes)
+app.delete("/api/emplotees/:employeeId?", employeeRoutes)
+app.put("/api/users/password", tokenVerification)
+app.get("/api/users/",tokenVerification)
+app.delete("/api/users/:userId?", tokenVerification)
+app.get("/api/users/user", tokenVerification)
+app.use("/api/users", userRoutes)
+app.put("/api/orders/:orderId", orderRoutes)
+app.use("/api/orders", orderRoutes)
+app.get("/api/orders", orderRoutes)
+app.use("/api/auth", authRoutes)
+
+module.exports = app;
