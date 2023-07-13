11c3147af1bbaddd581963bd22c55bc88095dea2
WojciechWnukk <wojciech.wnukk@gmail.com>
2023-07-13 13:25:15 +0200

    still

--- client/src/components/OrderRealize/index.jsx
+++ client/src/components/OrderRealize/index.jsx
@@ -76,7 +76,7 @@ const OrderRealize = ({ handleLogout }) => {
     console.log(token)
 
 
-    const apiUrl = "https://orca-app-4ec63.ondigitalocean.app/api/orders"
+    const apiUrl = "http://164.92.133.224/api/orders"
     try {
       const url = "http://localhost:8080/api/orders";
 
--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const app1 = express()
 const cors = require('cors')
 const userRoutes = require("./routes/users")
 const authRoutes = require("./routes/auth")
 const orderRoutes = require("./routes/orders")
 const employeeRoutes = require("./routes/employees")
 //middleware
-app.use(express.json())
-app.use(cors())
+app1.use(express.json())
+app1.use(cors())
 
 
 // Obsługa nagłówków CORS
-app.use((req, res, next) => {
+app1.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://restaurant-page-pink.vercel.app'); // Zmień na rzeczywisty adres klienta na Vercel
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -25,19 +25,19 @@ const connection = require('./db')
 connection()
 
 // routes
-app.use("/api/employees", employeeRoutes)
-app.delete("/api/emplotees/:employeeId?", employeeRoutes)
-app.put("/api/users/password", tokenVerification)
-app.get("/api/users/",tokenVerification)
+app1.use("/api/employees", employeeRoutes)
+app1.delete("/api/emplotees/:employeeId?", employeeRoutes)
+app1.put("/api/users/password", tokenVerification)
+app1.get("/api/users/",tokenVerification)
 //app.delete("/api/users", tokenVerification)
-app.delete("/api/users/:userId?", tokenVerification)
-app.get("/api/users/user", tokenVerification)
-app.use("/api/users", userRoutes)
-app.put("/api/orders/:orderId", orderRoutes)
-app.use("/api/orders", orderRoutes)
-app.get("/api/orders", orderRoutes)
-app.use("/api/auth", authRoutes)
+app1.delete("/api/users/:userId?", tokenVerification)
+app1.get("/api/users/user", tokenVerification)
+app1.use("/api/users", userRoutes)
+app1.put("/api/orders/:orderId", orderRoutes)
+app1.use("/api/orders", orderRoutes)
+app1.get("/api/orders", orderRoutes)
+app1.use("/api/auth", authRoutes)
 
 
 const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
+app1.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
