05a33bd5cd642ea714055e1152cce62903a78c47
WojciechWnukk <wojciech.wnukk@gmail.com>
2023-07-13 11:33:57 +0200

    still

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ const employeeRoutes = require("./routes/employees")
 app.use(express.json())
 app.use(cors())
 
+
+// Obsługa nagłówków CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://restaurant-page-pink.vercel.app'); // Zmień na rzeczywisty adres klienta na Vercel
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 const tokenVerification = require('./middleware/tokenVerification')
 
 const connection = require('./db')
